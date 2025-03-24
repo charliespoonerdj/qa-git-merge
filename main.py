1141b5f6ca2156191583604e521c2cbf9c21d1d6
Noyantis-JamieH <167077347+Noyantis-JamieH@users.noreply.github.com>
2025-03-24 14:04:07 +0000

    Jamie's Change

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 # A basic Python script that prints a message using variables
 
 # Define some variables
-greeting = "Hello"
-name = "Charlie"
-age = 27
+greeting = "Howdy"
+name = "Jamie"
+age = 19
 
 # Build a message using the variables
 message = f"{greeting}, my name is {name} and I am {age} years old."
